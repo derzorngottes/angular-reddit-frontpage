e20eb4f4e87ec5458ad600d261921a062a2d85de
derzorngottes <amy.conchie@gmail.com>
2016-05-05 14:14:00 -0700

    Add vote services

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ app.controller('myCtrl', ['$scope', function myCtrl($scope) {
   $scope.vm.newpost = false;
   $scope.vm.posts = [
     {
-      image: 'http://placehold.it/160x120',
+      image: 'http://lorempixel.com/160/120',
       title: 'Outside Aspen',
       author: 'ben111',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate',
@@ -14,7 +14,7 @@ app.controller('myCtrl', ['$scope', function myCtrl($scope) {
       votes: 0
     },
     {
-      image: 'http://placehold.it/160x120',
+      image: 'http://lorempixel.com/160/120',
       title: 'Nothing Good',
       author: 'ben121',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate',
@@ -23,7 +23,7 @@ app.controller('myCtrl', ['$scope', function myCtrl($scope) {
       votes: 0
     },
     {
-      image: 'http://placehold.it/160x120',
+      image: 'http://lorempixel.com/160/120',
       title: 'Rainbows',
       author: 'ben131',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate',
@@ -38,3 +38,11 @@ app.controller('myCtrl', ['$scope', function myCtrl($scope) {
 app.service('submitNow', function() {
   $scope.vm.newpost = true;
 });
+
+app.service('upvote', function(post) {
+  $scope.vm[post].votes += 1;
+});
+
+app.service('downvote', function(post) {
+  $scope.vm[post].votes -= 1;
+})
