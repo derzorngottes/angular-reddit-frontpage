7d1e645f3fa96731c7056db1f749edef03ee52a7
derzorngottes <amy.conchie@gmail.com>
2016-05-05 09:43:13 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var app = angular.module('myApp', []);
+
+app.controller('myCtrl', ['$scope', function myCtrl($scope) {
+  $scope.vm = {};
+  $scope.vm.posts = [
+    {
+      image: 'http://placehold.it/100x100',
+      title: 'Outside Aspen',
+      author: 'ben111',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate',
+      posted: 'date',
+      comments: 0,
+      votes: 0
+    },
+    {
+      image: 'http://placehold.it/100x100',
+      title: 'Nothing Good',
+      author: 'ben121',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate',
+      posted: 'date',
+      comments: 0,
+      votes: 0
+    },
+    {
+      image: 'http://placehold.it/100x100',
+      title: 'Rainbows',
+      author: 'ben131',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate',
+      posted: 'date',
+      comments: 0,
+      votes: 0
+    }
+  ];
+}]);
+
+
+app.service('something', function() {
+  //do something
+});
